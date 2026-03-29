231df62b47050751d572d77d3bc032c57d892e97
Rithu-4 <rithurajan2000@gmail.com>
2026-03-29 12:30:34 +0530

    updated project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import Navbar from "./component/Navbar";
+import Search from "./component/search";
+import Product from "./component/product";
+import About from "./component/About";
+import Footer from "./component/footer";
+
+function App(){
+    return(
+          <div>
+    <Navbar></Navbar>
+    <Search></Search>
+    <Product></Product>
+    <About></About>
+    <Footer></Footer>
+  </div>
+    )
 }
-
 export default App;
---
+++ src/component/Navbar.js
@@ -0,0 +1,15 @@
+
+function Navbar() {
+  return (
+    <nav>
+      <h1>perfumy</h1>
+      <ul>
+        <li>home|</li>
+        <li>product|</li>
+        <li>about|</li>
+        <li>contact|</li>
+      </ul>
+    </nav>
+  );
+}
+export default Navbar
